f43c32f37cc1147fb19fa0f2362d0faeff1ed39d
TheLazyTurtle <jcgodschalk@gmail.com>
2021-07-10 16:24:25 +0200

    feat(api reduction) Reduced the amount of songs requested in timeframe where user is often inactive

--- backOffice/src/inserter.py
+++ backOffice/src/inserter.py
@@ -9,99 +9,103 @@ class Inserter():
         self.userID = userID
         self.songs = []
 
-    def insertSong(self):
-        for song in self.songs:
-            values = {
-                "songID": song["songID"],
-                "name": song["name"],
-                "length": song["length"],
-                "url": song["url"],
-                "img": song["img"],
-                "preview": song["preview"]
-            }
+    def insertSong(self, song):
+        values = {
+            "songID": song["songID"],
+            "name": song["name"],
+            "length": song["length"],
+            "url": song["url"],
+            "img": song["img"],
+            "preview": song["preview"]
+        }
 
-            r = req.post(self.url + "song/create.php", data=values)
-            httpResponse = r.status_code
+        r = req.post(self.url + "song/create.php", data=values)
+        httpResponse = r.status_code
 
-            if httpResponse == 201:
-                printc("Added song:", "green", song["name"])
-            elif httpResponse == 503:
-                pass
-            else:
-                printc("Failed to add song:", "red",
-                       song["name"], "white", httpResponse, "white")
+        if httpResponse == 201:
+            printc("Added song:", "green", song["name"])
+        elif httpResponse == 503:
+            pass
+        else:
+            printc("Failed to add song:", "red",
+                   song["name"], "white", httpResponse, "white")
 
     # TODO: add img to artist
-    def insertArtist(self):
-        for song in self.songs:
-            for artist in song["artists"]:
-                values = {
-                    "artistID": artist["artistID"],
-                    "name": artist["name"],
-                    "url": artist["url"],
-                    "img": artist["img"]
-                }
-
-                r = req.post(self.url + "artist/create.php", data=values)
-                httpResponse = r.status_code
-
-                if httpResponse == 201:
-                    printc("Added artist:", "green", artist["name"])
-                elif httpResponse == 503:
-                    pass
-                else:
-                    printc("Failed to add artist:", "red",
-                           artist["name"], "white", httpResponse)
-
-    def markAsPlayed(self):
-        for song in self.songs:
+    def insertArtist(self, song):
+        for artist in song["artists"]:
             values = {
-                "songID": song["songID"],
-                "datePlayed": song["playedAt"],
-                "playedBy": self.userID,
-                "songName": song["name"]
+                "artistID": artist["artistID"],
+                "name": artist["name"],
+                "url": artist["url"],
+                "img": artist["img"]
             }
 
-            r = req.post(self.url + '/played/create.php', data=values)
+            r = req.post(self.url + "artist/create.php", data=values)
             httpResponse = r.status_code
 
             if httpResponse == 201:
-                printc("Added song as played", "green",
-                       song["name"], "white", self.userID, "white")
+                printc("Added artist:", "green", artist["name"])
             elif httpResponse == 503:
                 pass
             else:
-                printc("Failed to add song as played:", "red",
-                       song["name"], "white", self.userID, "white")
+                printc("Failed to add artist:", "red",
+                       artist["name"], "white", httpResponse)
 
-    def linkArtistToSong(self):
-        for song in self.songs:
-            for artist in song["artists"]:
-                values = {
-                    "songID": song["songID"],
-                    "artistID": artist["artistID"]
-                }
+    def markAsPlayed(self, song):
+        values = {
+            "songID": song["songID"],
+            "datePlayed": song["playedAt"],
+            "playedBy": self.userID,
+            "songName": song["name"]
+        }
 
-                r = req.post(self.url + "song/linkArtistToSong.php",
-                             data=values)
-                httpResponse = r.status_code
+        r = req.post(self.url + '/played/create.php', data=values)
+        httpResponse = r.status_code
 
-                if httpResponse == 201:
-                    printc("Linked artist to song", "green",
-                           artist["name"] + " - " + song["name"], "white")
-                elif httpResponse == 503:
-                    pass
-                else:
-                    printc("Failed to link artist to song", "red",
-                           artist["name"] + " - " + song["name"], "white")
+        if httpResponse == 201:
+            printc("Added song as played", "green",
+                   song["name"], "white", self.userID, "white")
+            return True
+        elif httpResponse == 503:
+            pass
+        else:
+            printc("Failed to add song as played:", "red",
+                   song["name"], "white", self.userID, "white")
+
+    def linkArtistToSong(self, song):
+        for artist in song["artists"]:
+            values = {
+                "songID": song["songID"],
+                "artistID": artist["artistID"]
+            }
+
+            r = req.post(self.url + "song/linkArtistToSong.php",
+                         data=values)
+            httpResponse = r.status_code
+
+            if httpResponse == 201:
+                printc("Linked artist to song", "green",
+                       artist["name"] + " - " + song["name"], "white")
+            elif httpResponse == 503:
+                pass
+            else:
+                printc("Failed to link artist to song", "red",
+                       artist["name"] + " - " + song["name"], "white")
 
     def run(self, songs):
-        if not isinstance(songs, bool):
-            self.songs = songs
-        else:
+        # If songs is an boolean than it didn't get an result and print an error
+        if isinstance(songs, bool):
             printc("Failed to get songs:", "red", self.userID, "white")
+        else:
+            self.songs = songs
+            added = 0
 
-        self.insertSong()
-        self.insertArtist()
-        self.linkArtistToSong()
-        self.markAsPlayed()
+            for song in self.songs:
+                if self.markAsPlayed(song):
+                    added += 1
+                self.insertSong(song)
+                self.insertArtist(song)
+                self.linkArtistToSong(song)
+
+            if added == len(self.songs):
+                return True
--- backOffice/src/user.py
+++ backOffice/src/user.py
@@ -1,8 +1,12 @@
 import threading
 import fetcher
 import inserter
+import requests as req
+import json
 from time import sleep
+from datetime import datetime
 from creds import apiUrl
+from printer import printc
 
 
 class User():
@@ -10,18 +14,70 @@ class User():
         self.userID = userID
 
     # This will fetch the songs from a user
-    # TODO: Add error handling and printing
     def fetch(self):
+        # Create the fetcher and inserter for the user
         self.fetcher = fetcher.Fetcher(self.userID)
         self.inserter = inserter.Inserter(self.userID, apiUrl)
 
+        # Set default fetch values
+        amount, time = self.getTimeAndAmount()
+
         while True:
             # This returns the songs objects and insert them into the db
-            songs = self.fetcher.run()
-            self.inserter.run(songs)
+            try:
+                songs = self.fetcher.run(amount)
+                if self.inserter.run(songs):
+                    # If the amound added as played == the amount fetched than fetch again but than the max amount of songs to prevent any songs to get lost
+                    printc("Refetching to prevent loss of songs for:",
+                           "green", self.userID, "white")
+                    songs = self.fetcher.run(50)
+                    self.inserter.run(songs)
 
-            # TODO: Implement GH issue #43
-            sleep(300)
+            except Exception as e:
+                printc("Failed to get songs for:", "red",
+                       self.userID, "white", e, "white")
+
+            sleep(time)
+
+    def getTimeAndAmount(self):
+        # Get the current time and active rate of the user
+        hours = self.getActiveHours()
+        currentHour = self.getCurrentTime()
+
+        amount = 50
+        time = 3600
+
+        for hour in hours:
+            if hour["time"] == currentHour:
+                percent = round(float(hour["percentage"]))
+
+                if percent <= 1:
+                    amount = 50
+                    time = 3600
+                elif percent > 1 and percent <= 3:
+                    amount = 25
+                    time = 1800
+                elif percent > 3:
+                    amount = 5
+                    time = 300
+
+        printc("Fetching " + str(amount) + " over " + str(time) +
+               " seconds for:", "green", self.userID, "white")
+        return amount, time
+
+    def getActiveHours(self):
+        r = req.get(apiUrl + "user/getActiveHours.php",
+                    params={"userID": self.userID})
+        hours = r.text
+        hours = json.loads(hours)
+        hours = hours['records']
+        return hours
+
+    def getCurrentTime(self):
+        now = datetime.now()
+
+        current_time = now.strftime("%H")
+        return current_time
 
     # This will run all the tasks that need to be run for a user
     def run(self):
