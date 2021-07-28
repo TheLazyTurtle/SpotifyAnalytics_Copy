cc6f3cbb28d5669bd3cb7b9149f4052f7ff94ba6
TheLazyTurtle <jcgodschalk@gmail.com>
2021-07-28 11:37:20 +0200

    feat(header) Added logout button

--- header.php
+++ header.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+$url = $_SERVER["REQUEST_URI"];
+$url = str_replace("/", '', $url);
+$url = str_replace(".php", '', $url);
 ?>
 
 <!DOCTYPE html>
@@ -20,16 +23,22 @@ session_start();
 	<script src="/js/check_jwt.js"></script>
 </head>
 
-<header>
-	<div class="header">
-		<div class="col col-left">
-			<button class="btn" onclick="window.location.href='/index.php'">Home</button>
+<?php
+if ($url != "login" && $url != "register") {
+?>
+	<header>
+		<div class="header">
+			<div class="col col-left">
+				<button class="btn" onclick="window.location.href='/index.php'">Home</button>
+			</div>
+			<div class="col col-middel">
+				<input id="search-box" type="text" name="Search" placeholder="search">
+			</div>
+			<div class="col col-right">
+				<button class="btn" onclick="window.location.href='/logout.php'">Uitloggen</button>
+			</div>
 		</div>
-		<div class="col col-middel">
-			<input id="search-box" type="text" name="Search" placeholder="search">
-		</div>
-		<div class="col col-right">
-			<button class="btn" onclick="window.location.href='/logout.php'">Uitloggen</button>
-		</div>
-	</div>
-</header>
+	</header>
+<?php
+}
+?>
---
+++ logout.php
@@ -0,0 +1,17 @@
+<?php
+require "header.php";
+
+session_unset();
+session_destroy();
+
+// This removes all the coockies
+if (isset($_SERVER['HTTP_COOKIE'])) {
+	$cookies = explode(';', $_SERVER["HTTP_COOKIE"]);
+
+	foreach ($cookies as $cookie) {
+		$parts = explode('=', $cookie);
+		$name = trim($parts[0]);
+		setcookie($name, '', time() - 1000);
+		setcookie($name, '', time() - 1000, '/');
+	}
+}
