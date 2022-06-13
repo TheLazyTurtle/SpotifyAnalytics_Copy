8047b6382f436314204237f70f0a5257c8cf2e43
TheLazyTurtle <jcgodschalk@gmail.com>
2022-06-13 19:51:02 +0200

    Preparation to merge with react

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,10 +16,9 @@
     <div id="root"></div>
     <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
     <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js" crossorigin></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
-        crossorigin="anonymous"></script>
-    <script src="{{ asset('static/js/main.eaad0c19.js') }}"></script>
-    <link href="{{ asset('static/css/main.c02fa724.css')}}" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script src="{{ asset('../js/app.js') }}"></script>
+    <link href="{{ asset('../css/app.css')}}" rel="stylesheet">
 </body>
+
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,21 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            //adding react and react-dom may not be necessary for you but it did fix some issues in my setup.
+            'react': path.resolve('node_modules/react'),
+            'react-dom': path.resolve('node_modules/react-dom'),
+        },
+    },
+});
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.react('resources/js/app.js', 'public/js')
+    .extract(['react', 'react-dom']);
+mix.postCss('resources/css/app.css', 'public/css', [])
+mix.postCss('resources/css/app.css', 'public/css', [])
+// mix.js('resources/js/app.js', 'public/js')
+//     .postCss('resources/css/app.css', 'public/css', [
+//         //
+    // ]);
